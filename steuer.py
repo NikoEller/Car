1753d70f47e40c10a04a7aec5b420c131b8e20db
NIko Eller <niko.eller@live.com>
2019-04-11 13:57:13 +0200

    Update
    PIn from board to bcm

--- steuer.py
+++ steuer.py
@@ -4,15 +4,15 @@ import time
 
 a = ""
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+Motor1A = 23
+Motor1B = 24
+Motor1E = 25
 
-Motor2A = 23
-Motor2B = 21
-Motor2E = 19
+Motor2A = 11
+Motor2B = 9
+Motor2E = 10
 
 GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
@@ -114,61 +114,23 @@ def stop():
 
 
 
-
-def distance():
-    # setze Trigger auf HIGH
-    GPIO.output(GPIO_TRIGGER2, True)
-
-    # setze Trigger nach 0.01ms aus LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER2, False)
-
-    StartZeit2 = time.time()
-    StopZeit2 = time.time()
-
-    # speichere Startzeit
-    while GPIO.input(GPIO_ECHO2) == 0:
-        StartZeit2 = time.time()
-
-    # speichere Ankunftszeit
-    while GPIO.input(GPIO_ECHO2) == 1:
-        StopZeit2 = time.time()
-
-    # Zeit Differenz zwischen Start und Ankunft
-    TimeElapsed2 = StopZeit2 - StartZeit2
-    # mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
-    # und durch 2 teilen, da hin und zurueck
-    distanz2 = (TimeElapsed2 * 34300) / 2
-
-    GPIO.setmode(GPIO.BOARD)
-
-    print(distanz2)
-
-
-
 if __name__ == '__main__':
     try:
         while True:
             a = raw_input()
             if(a == "w"):
                 goforward()
-                distance()
             if(a == "a"):
                 turn()
-                distance()
             if(a == "d"):
                 turn1()
-                distance()
             if(a == "s"):
                 backwards()
-                distance()
             if(a == "p"):
-                distance()
                 stop()
                 break
             if(a == "e"):
                 doubleturn()
-                distance()
 
 
 
